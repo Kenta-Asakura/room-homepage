134ff04e148940367909bfbe661419065d465557
kenta asakura <asakura.kenta963420@gmail.com>
2023-09-25 21:53:43 +0900

    Globalstyles (body) added and component included in App.js

--- src/App.js
+++ src/App.js
@@ -5,21 +5,26 @@ import GlobalStyles from "./components/styles/Global";
 
 const theme = {
   colors: {
-    'Dark Gray': '0, 0%, 63%',
-    'Black': '0, 0%, 0%',
-    'White': '0, 0%, 100%',
-    'Very Dark Gray': '0, 0%, 27%'
+    "Dark Gray": "0, 0%, 63%",
+    Black: "0, 0%, 0%",
+    White: "0, 0%, 100%",
+    "Very Dark Gray": "0, 0%, 27%",
   },
   fonts: {
-    light: '500',
-    medium: '600',
-    heavy: '700'
+    light: "500",
+    medium: "600",
+    heavy: "700",
   },
-  mobile: '375px'
+  mobile: "375px",
 };
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <GlobalStyles />
+      Hello World
+    </div>
+  );
 }
 
 export default App;
--- src/components/styles/Global.js
+++ src/components/styles/Global.js
@@ -1,10 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-@import url('https://fonts.google.com/specimen/League+Spartan')
+@import url('https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@500&family=League+Spartan:wght@500;600;700&family=Montserrat&display=swap');
 
 body {
-  font-family: 'League Spartan';
+  font-family: 'League Spartan', sans-serif;
   font-size: 12px;
 }
 `;
