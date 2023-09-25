9753c22282738f5fe46e568be277ca051e6af2d5
kenta asakura <asakura.kenta963420@gmail.com>
2023-09-25 22:09:49 +0900

    Header.js created (component not being used in App.js yet)

---
+++ src/components/styles/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <header>
+        <div>
+          <img src='' alt='' />
+        </div>
+
+        <nav></nav>
+
+        <div>
+          <button>
+            <img src='' alt='' />
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+}
