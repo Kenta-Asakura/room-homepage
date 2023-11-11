52746f79080cf3087f06eb6fcb892762469caf1c
kenta asakura <asakura.kenta963420@gmail.com>
2023-11-11 23:29:12 +0900

    header style on process

--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -1,41 +1,55 @@
 import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
-  width: 100%;
   position: absolute;
   z-index: 10;
   width: 100%;
   display: flex;
+  justify-content: start;
   align-items: center;
-  justify-content: center;
   flex-wrap: wrap;
+  padding: 32px;
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    align-items: center;
+    justify-content: center;
+  }
 `;
 
 export const HamburgerMenu = styled.div`
-  position: absolute;
-  top: 32px;
-  left: 32px;
-  z-index: 20;
+  display: none;
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    display: block;
+    position: absolute;
+    top: 32px;
+    left: 32px;
+    z-index: 20;
+  }
 `;
 
 export const Nav = styled.nav`
-  background: white;
   top: 0;
   left: 0;
   width: 100%;
+  // padding: 32px 0;
 
     & ul {
       display:flex;
       align-items: center;
       justify-content: center;
-      // flex-wrap: wrap;
+      flex-wrap: wrap;
       overflow: hidden;
 
       & li {
         list-style: none;
 
+      & li:hover {
+        border-bottom:
+      }
+
         button {
-          color: black;
+          color: white;
           background-color: transparent;
           // padding: 50px;
           border: none;
@@ -45,5 +59,5 @@ export const Nav = styled.nav`
 `;
 
 export const NavLi = styled.li`
-
+    color: white;
 `;
